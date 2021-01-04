cb5d572960ba63c0e20a97a44f4d8c632a510371
Shiyan Boxer <shiyanboxer7@gmail.com>
2021-01-04 11:23:11 -0500

    update

--- my-app/src/Components/Footer.js
+++ my-app/src/Components/Footer.js
@@ -1,8 +1,20 @@
-        // <a
-        //   className="App-link"
-        //   href="http://shiyanboxer.netlify.app/"
-        //   target="_blank"
-        //   rel="noopener noreferrer"
-        // >
-        //   By: Shiyan Boxer
-        // </a>
+import React from "react"
+
+class Footer extends React.Component{
+    render() {
+        return (
+            <a
+              className="Footer"
+              href="http://shiyanboxer.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              By: Shiyan Boxer
+            </a>
+        );
+    }
+}
+
+export default Footer;
+
+
