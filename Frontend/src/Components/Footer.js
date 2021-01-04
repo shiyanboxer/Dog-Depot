bdd95020b4fc36008f2bd586bd8631eb9db648d0
Shiyan Boxer <shiyanboxer7@gmail.com>
2021-01-04 13:08:54 -0500

    update styling for nav bar, cards, and footer

---
+++ Frontend/src/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+class Footer extends React.Component{
+    render() {
+        return (
+            <a
+              className="footer"
+              href="http://shiyanboxer.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              By: Shiyan Boxer
+            </a>
+        );
+    }
+}
+
+export default Footer;
---
+++ Frontend/src/Components/Routes.js
@@ -0,0 +1,17 @@
+import React from "react"
+import {Route, Switch} from "react-router-dom"
+import Home from "./Home"
+
+class Routes extends React.Component{
+    render() {
+        return (
+            <div>
+                <Switch>
+                <Route exact path="/" component={Home}></Route>
+                </Switch>
+            </div>
+        );
+    }
+}
+
+export default Routes;
